ae6ea00df7e28dc2c7eec718002fc1368e8ec2d0
syiwah <insyirah_24000157@utp.edu.my>
2024-06-02 08:50:38 +0800

    Add files via upload

---
+++ Insyirah - Java Chip zip/Item.java
@@ -0,0 +1,13 @@
+public class Item {
+    String name;
+    double supplierPrice;
+    int quantity;
+    String supplierName;
+
+    public Item(String name, double supplierPrice, int quantity, String supplierName) {
+        this.name = name;
+        this.supplierPrice = supplierPrice;
+        this.quantity = quantity;
+        this.supplierName = supplierName;
+    }
+}
