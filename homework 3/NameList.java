146541a2a6327203daaa8c3a53e875e19c7f79d9
syiwah <insyirah_24000157@utp.edu.my>
2024-07-09 23:30:45 +0800

    Create NameList.java

---
+++ homework 3/NameList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList ;
+ 
+Public class NameList
+ 
+private ArrayList< Name> names = new ArrayList<Name> ( ) ;
+ 
+public NameList( )
+{
+this.names = ArrayList <>() ;  
+ 
+public void addName(Name name) {
+names.add(name) ;
+ }
+}
