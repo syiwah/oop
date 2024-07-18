fb6f2d4953d52f17a6c5bbc9a73dbfc3676b0d03
syiwah <insyirah_24000157@utp.edu.my>
2024-07-18 21:26:19 +0800

    Update Java_Chip.sql

--- homework 4/Java_Chip.sql
+++ homework 4/Java_Chip.sql
@@ -7,6 +7,8 @@
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
+
+/* INSYIRAH BINTI HAMZAH 24000157*/
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
